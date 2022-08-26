84d5c82da0f9a811cd7a928c76cc73e3e196ea2c
russ <maetineruss@gmail.com>
2022-08-25 19:55:08 -0700

    first commit

---
+++ side-scroller/GameScene.swift
@@ -0,0 +1,49 @@
+//
+//  GameScene.swift
+//  side-scroller
+//
+//  Created by russell martinez on 8/25/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    
+    override func sceneDidLoad() {
+
+        self.lastUpdateTime = 0
+        
+    }
+    
+    func touchDown(atPoint pos : CGPoint) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        self.lastUpdateTime = currentTime
+    }
+}
