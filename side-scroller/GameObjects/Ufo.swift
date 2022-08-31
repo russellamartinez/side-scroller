3832d6438ae031a92c82afc5667208e5a8519712
russ <maetineruss@gmail.com>
2022-08-31 16:50:36 -0700

    refactored gameobjects

--- side-scroller/GameObjects/Ufo.swift
+++ side-scroller/GameObjects/Ufo.swift
@@ -12,5 +12,11 @@ class Ufo : SKSpriteNode
     convenience init()
     {
         self.init(imageNamed: "ufo")
+        self.name = "ufo"
+        self.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(8.0))
+        self.physicsBody?.affectedByGravity = false
+        self.physicsBody?.allowsRotation = false
+        self.physicsBody?.contactTestBitMask = PhysicsCategory.player | PhysicsCategory.collectable
+        self.physicsBody?.categoryBitMask = PhysicsCategory.ufo
     }
 }
