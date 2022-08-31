43e0a395fa1577a4e28cf5f28a27d5c4de8ab9fe
russ <maetineruss@gmail.com>
2022-08-30 22:21:41 -0700

    added ufo with tracking to player position

--- side-scroller/Enums/PhysicsCategory.swift
+++ side-scroller/Enums/PhysicsCategory.swift
@@ -10,6 +10,7 @@ enum PhysicsCategory {
     static let none:            UInt32 = 0
     static let player:          UInt32 = 0b1        // 1
     static let wall:            UInt32 = 0b10       // 2
-    static let collectable:     UInt32 = 0b100      // 3
-    static let fireball:        UInt32 = 0b1000     // 4
+    static let collectable:     UInt32 = 0b100      // 4
+    static let fireball:        UInt32 = 0b1000     // 8
+    static let ufo:             UInt32 = 0b10000    // 16
 }
